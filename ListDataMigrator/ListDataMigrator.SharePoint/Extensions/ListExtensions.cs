48641a534bcb401cfe8ea4e1f31415f387a2e5cb
Clark Tozer <clarkwtozer@gmail.com>
2018-08-02 22:04:49 +1200

    list items extensions

---
+++ ListDataMigrator/ListDataMigrator.SharePoint/Extensions/ListExtensions.cs
@@ -0,0 +1,35 @@
+﻿using CamlexNET;
+using Microsoft.SharePoint.Client;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListDataMigrator.SharePoint.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<ListItem> GetAllItemsPaged(this List list, List<string> viewFields = null, int rowLimit = 500)
+        {
+            var allItems = new List<ListItem>();
+            var caml = Camlex.Query();
+
+            if (viewFields != null && viewFields.Any())
+            {
+                caml.ViewFields(viewFields.ToArray());
+            }
+            
+            caml.Take(rowLimit).Scope(ViewScope.RecursiveAll);
+
+            var camlQuery = caml.ToCamlQuery();
+            do
+            {
+                var queriedItems = list.GetItems(camlQuery);
+                list.Context.Load(queriedItems);
+                list.Context.ExecuteQuery();
+                camlQuery.ListItemCollectionPosition = queriedItems.ListItemCollectionPosition;
+                allItems.AddRange(queriedItems.ToList());
+            } while (camlQuery.ListItemCollectionPosition != null);
+
+            return allItems;
+        }
+    }
+}
---
+++ ListDataMigrator/ListDataMigrator.SharePoint/Services/ListExporter.cs
@@ -0,0 +1,41 @@
+﻿using ListDataMigrator.Common.Extensions;
+using ListDataMigrator.SharePoint.Extensions;
+using Microsoft.SharePoint.Client;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ListDataMigrator.SharePoint.Services
+{
+    public class ListExporter
+    {
+        private Web _web;
+        private string _listTitle;
+        private List<string> _viewFields;
+
+        public ListExporter(Web web, string listTitle, List<string> viewFields)
+        {
+            _web = web;
+            _listTitle = listTitle;
+            _viewFields = viewFields;
+        }
+
+        public void ExportItems(string path, string fieldNameKeyBy = null)
+        {
+            var jsonSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var list = _web.Lists.GetByTitle(_listTitle);
+            var allItems = list.GetAllItemsPaged(_viewFields);
+
+            var fieldValues = allItems.Select(item => item.FieldValues);
+
+            var file = Path.Combine(path, _listTitle.GetSafeFilename());
+            var serialized = JsonConvert.SerializeObject(fieldValues, Formatting.Indented, jsonSettings);
+            System.IO.File.WriteAllText(file, serialized);
+        }
+    }
+}
